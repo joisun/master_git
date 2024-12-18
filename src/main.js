0dd18017aca9cdb656f9eb755704c23e899e8ac3
shuizhenyang <shuizhenyang@qipeng.com>
2024-12-18 10:55:55 +0800

    fix: http to https

--- src/main.js
+++ src/main.js
@@ -1,60 +1,64 @@
-import Vue from 'vue'
-import Element from 'element-ui'
-import Router from 'vue-router'
-import Resource from 'vue-resource'
+import Vue from "vue";
+import Element from "element-ui";
+import Router from "vue-router";
+import Resource from "vue-resource";
 
-import tools from './global/function/interface-tool'
-import * as plugins from './plugins'
-import './global/index'
-import api from '@/api/ajax'
-import store from './store'
-import enums from '@/constant/enums'
-import Vlf from 'vlf'
-import localforage from 'localforage'
-import { commonMessageConfig } from './plugins'
-import { getAuthAndAddRoutes } from './permission'
+import tools from "./global/function/interface-tool";
+import * as plugins from "./plugins";
+import "./global/index";
+import api from "@/api/ajax";
+import store from "./store";
+import enums from "@/constant/enums";
+import Vlf from "vlf";
+import localforage from "localforage";
+import { commonMessageConfig } from "./plugins";
+import { getAuthAndAddRoutes } from "./permission";
 import App from "@/page/app.vue";
 
-if (window.location.protocol === 'http:' && window.location.host.indexOf('test') === -1) {
-  window.location.href = window.location.href.replace('http:', 'https:');
+if (
+  window.location.protocol === "http:" &&
+  window.location.host.indexOf("test") === -1 &&
+  window.location.host.indexOf("localhost") === -1
+) {
+  window.location.href = window.location.href.replace("http:", "https:");
 }
 
-Vue.use(Router)
-Vue.mixin(tools)
-Element.Dialog.props.closeOnClickModal.default = false
+Vue.use(Router);
+Vue.mixin(tools);
+Element.Dialog.props.closeOnClickModal.default = false;
 Element.Select.props.filterable = {
   type: Boolean,
-  default: true
-}
+  default: true,
+};
 Vue.use(Element, {
-  size: 'small'
-})
-const Message = Element.Message
+  size: "small",
+});
+const Message = Element.Message;
 const $message = (options) => {
   return Message({
     ...commonMessageConfig(options.type),
-    ...options
-  })
-}
+    ...options,
+  });
+};
 
 //重写一遍success的方法,将offset写入options
-;['success', 'warning', 'info', 'error'].forEach((type) => {
+["success", "warning", "info", "error"].forEach((type) => {
   $message[type] = (options) => {
-    if (typeof options === 'string') {
+    if (typeof options === "string") {
       options = {
         ...commonMessageConfig(type),
-        message: options
-      }
+        message: options,
+      };
     }
-    options.type = type
+    options.type = type;
     return Message({
       ...commonMessageConfig(options.type),
-      ...options
-    })
-  }
-})
+      ...options,
+    });
+  };
+});
 //将$message挂载到this上
-Vue.prototype.$message = $message
+Vue.prototype.$message = $message;
 
 // eslint-disable-next-line no-unused-vars
 for (let p of [
@@ -65,55 +69,55 @@ for (let p of [
   plugins.confirmPlugin,
   plugins.notifyPlugin,
   plugins.variablePlugin,
-  plugins.storagePlugin
+  plugins.storagePlugin,
 ]) {
-  Vue.use(p)
+  Vue.use(p);
 }
 
-Vue.use(Vlf, localforage)
+Vue.use(Vlf, localforage);
 
 // 热重载配置
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
 
 function initApp(router) {
   return new Vue({
     // el: '#app',
     router,
-    store
+    store,
   }).$mount("#app");
 }
 
 function initEnum() {
   return new Promise((resolve) => {
     api.common.getEnum().then((resp) => {
-      if (!resp) return
+      if (!resp) return;
       const finalEnums = {
         ...resp.data,
         platformTypes: {
           cmcc: {
-            name: '移动',
-            value: 'cmcc',
-            platforms: ['PBOSS', 'CTBOSS', '5G', 'NB', 'INTERNATION']
+            name: "移动",
+            value: "cmcc",
+            platforms: ["PBOSS", "CTBOSS", "5G", "NB", "INTERNATION"],
           },
           unicom: {
-            name: '联通',
-            value: 'unicom',
-            platforms: ['Jasper', '5G', 'NB', 'INTERNATION']
+            name: "联通",
+            value: "unicom",
+            platforms: ["Jasper", "5G", "NB", "INTERNATION"],
           },
           chinanet: {
-            name: '电信',
-            value: 'chinanet',
-            platforms: ['CMP', 'DCP', '5G', 'NB', 'INTERNATION']
-          }
-        }
-      }
-      store.commit('setEnum', finalEnums) // 原来存了一份到store，保持不变
-      enums.install(finalEnums)
-      resolve(true)
-    })
-  })
+            name: "电信",
+            value: "chinanet",
+            platforms: ["CMP", "DCP", "5G", "NB", "INTERNATION"],
+          },
+        },
+      };
+      store.commit("setEnum", finalEnums); // 原来存了一份到store，保持不变
+      enums.install(finalEnums);
+      resolve(true);
+    });
+  });
 }
 
-initEnum().then(getAuthAndAddRoutes).then(initApp)
+initEnum().then(getAuthAndAddRoutes).then(initApp);
